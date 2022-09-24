fba763f7f70050162e2a682d5f6ea0ad86537a2d
Matheus Feres Turcheti <71662383+matheus-ft@users.noreply.github.com>
2022-09-24 16:01:56 -0300

    Remove unused stuff and add activation vector to ann

--- ANN-MNIST/ann.py
+++ ANN-MNIST/ann.py
@@ -1,25 +1,23 @@
 import numpy as np
 import numpy.random as npr
 from scipy.special import expit as sigmoid  # faster than using np.exp
+import utils as ut
 
 
 class ANN:
     def __init__(
         self,
-        n_features: int,
         n_classes: int,
-        n_hidden_neurons: int,
+        n_neurons_per_layer: int,
         n_hidden_layers: int = 1,
     ) -> None:
-        self.__number_features = n_features
+        ### Architecture
         self.__number_classes = n_classes
         self.__number_hidden_layers = n_hidden_layers
-        self.__number_neurons: list[int] = self._neurons_list(n_hidden_neurons)
-        self._weights: list[np.ndarray] = self._initialize_weights()
-
-    @property
-    def number_features(self) -> int:
-        return self.__number_features
+        self.__neurons_per_layer: list[int] = self._neurons_list(n_neurons_per_layer)
+        ### Data
+        self._weights: list[np.matrix] = self._initialize_weights()
+        self._activation = [ut.vector(np.zeros(n_l)) for n_l in self.neurons_per_layer]
 
     @property
     def number_classes(self) -> int:
@@ -30,42 +28,29 @@ class ANN:
         return self.__number_hidden_layers
 
     @property
-    def number_neurons(self) -> list[int]:
-        return self.__number_neurons
+    def neurons_per_layer(self) -> list[int]:
+        return self.__neurons_per_layer
 
     @property
-    def weights(self) -> list[np.ndarray]:
+    def weights(self) -> list[np.matrix]:
         return self._weights
 
-    def _neurons_list(self, n_hidden_neurons: int) -> list[int]:
-        neurons_list = [self.number_features]
-        neurons_list.extend([n_hidden_neurons] * self.number_hidden_layers)
+    @property
+    def activation(self) -> list[ut.vector]:
+        return self._activation
+
+    def _neurons_list(self, n_neurons_per_layer: int) -> list[int]:
+        neurons_list = [n_neurons_per_layer] * self.number_hidden_layers
         neurons_list.append(self.number_classes)
         return neurons_list
 
-    def _initialize_weights(self) -> list[np.ndarray]:
+    def _initialize_weights(self) -> list[np.matrix]:
         weights = []
         for i in range(self.number_hidden_layers + 1):
-            neurons_left = self.number_neurons[i]
-            neurons_right = self.number_neurons[i + 1]
+            neurons_left = self.neurons_per_layer[i]
+            neurons_right = self.neurons_per_layer[i + 1]
             epislon = (6 / (neurons_left + neurons_right)) ** 0.5
-            random_theta = npr.rand(neurons_left, neurons_right + 1)  # +1 due to bias
+            random_theta = npr.rand(neurons_right, neurons_left + 1)  # +1 due to bias
             weights.append(random_theta * 2 * epislon - epislon)
         return weights
 
-    def _forward_pass(self):
-        pass
-
-    def _back_propagation(self):
-        pass
-
-    def fit(examples: np.ndarray, labels: np.ndarray) -> np.ndarray:
-        pass
-
-    def predict(new_examples: np.ndarray) -> np.ndarray:
-        pass
-
-
-def sigmoid_prime(z: float) -> float:
-    a = sigmoid(z)
-    return a * (1 - a)
