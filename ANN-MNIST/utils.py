1942f9ec3da075880b867aa34ad0eebf42428268
Matheus Feres Turcheti <71662383+matheus-ft@users.noreply.github.com>
2022-09-25 12:47:21 -0300

    Add cross entropy functions

--- ANN-MNIST/utils.py
+++ ANN-MNIST/utils.py
@@ -37,5 +37,32 @@ def sigmoid_prime(z: float | np.ndarray):
     return a * (1 - a)
 
 
-def cross_entropy(y: np.matrix) -> float:
-    pass
+def _binary_cross_entropy(label: np.ndarray, prediction: np.ndarray) -> float:
+    if len(label.shape) > 1 or len(prediction.shape) > 1:
+        raise Exception("Use the funtion `cross_entropy` if there's more than 1 class")
+
+    m = label.shape[0]  # number of examples
+    odd_yes = label * np.log(prediction)
+    odd_no = (1 - label) * np.log(1 - prediction)
+    costs = odd_yes + odd_no
+    J = sum(costs)
+    return -J / m
+
+
+def cross_entropy(
+    label: np.ndarray | np.matrix, prediction: np.ndarray | np.matrix
+) -> float:
+    if len(label.shape) == 1 == len(prediction.shape):
+        return _binary_cross_entropy(label, prediction)
+    elif label.shape != prediction.shape:
+        raise Exception("`label` and `prediction` must have the same dimensions")
+
+    m = label.shape[0]  # number of examples
+    K = label.shape[1]  # number of classes
+    J = 0
+    for k in range(K):
+        odd_yes = label[:, k] * np.log(prediction[:, k])
+        odd_no = (1 - label[:, k]) * np.log(1 - prediction[:, k])
+        costs = odd_yes + odd_no
+        J += sum(costs)
+    return -J / m
