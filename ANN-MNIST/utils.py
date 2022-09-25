6cb7692d6df054d53572416286b3e2249817a5e5
Matheus Feres Turcheti <71662383+matheus-ft@users.noreply.github.com>
2022-09-25 11:12:26 -0300

    Refactor utililities module

--- ANN-MNIST/utils.py
+++ ANN-MNIST/utils.py
@@ -1,57 +1,41 @@
 import numpy as np
-from scipy.special import expit as sigmoid  # faster than using np.exp
+from scipy.special import expit  # np.exp is slower and math.exp can't receive array
 
 
-class vector(np.ndarray):
-    """2-dimensional numpy array representing a column vector.
-
-    Extends the numpy.ndarray class to be able to treat arrays as "proper" vector in R^n.
+def sigmoid(z: float | np.ndarray):
+    """Gives the sigmoid logistic function value on z.
 
     Parameters
     ----------
-    lst : list[float]
-        iterable from which the vector is created
+    z : float | np.ndarray
+        Argument, or array of arguments, for the logistic function
+
+    Returns
+    -------
+    float | np.ndarray
+        Returns the numerical value of the logistic function, or an np.ndarray of such values
     """
-
-    def __new__(cls, lst: list[float]):
-        v = np.array([lst]).reshape(len(lst), 1)  # the value that actually matters
-        v = np.asarray(v).view(cls)  # to make it behave as its own "data type"
-        return v
+    return expit(z)
 
 
-class matrix(np.ndarray):
-    """2-dimensional numpy array representing a matrix.
-
-    Extends the numpy.ndarray class to be able to build a matrix either by a list of lists or by a list of column vectors.
-
-    Parameters
-    ----------
-    lst : list[list[float]] | list[vector]
-        list containing list[float]s or vectors of floats
-    """
-
-    def __new__(cls, lst: list[list[float]] | list[vector]):
-        m = np.array(lst)
-        if type(lst[0]) == vector:
-            m = np.concatenate(m, axis=1)
-        m = np.asarray(m).view(cls)  # to make it behave as its own "data type"
-        return m
-
-
-def sigmoid_prime(z: float) -> float:
-    """Gives the derivative of the sigmoid logistic function g.
+def sigmoid_prime(z: float | np.ndarray):
+    """Gives the derivative of the sigmoid logistic function g:
 
     g'(z) = g(z) * [1 - g(z)]
 
     Parameters
     ----------
-    z : float
-        Argument passed to the function. Can be an np.ndarray of floats and will operate coordinate-wise.
+    z : float | np.ndarray
+        Argument, or array of arguments, for the derivative of the logistic function
 
     Returns
     -------
-    float
+    float | np.ndarray
         Returns the numerical value of the derivative of the logistic function, or an np.ndarray of such values
     """
-    a = sigmoid(z)
+    a = expit(z)
     return a * (1 - a)
+
+
+def cross_entropy(y: np.matrix) -> float:
+    pass
