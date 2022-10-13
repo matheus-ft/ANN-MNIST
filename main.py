9fd4e78062fd6e15d65a58b229d0309edefc95c9
p2o <ricardo@p2o.com.br>
2022-10-13 13:02:21 -0300

    first ricardo's commit

--- main.py
+++ main.py
@@ -40,28 +40,3 @@ print("tempo:",dt.now() - agora)
 
 pred = prediction(X,nn)
 print("Training Set Accuracy:",sum(pred[:,np.newaxis]==y_t)[0]/5000*100,"%")
-
-"""
-initial_theta1 = randInitializeWeights(input_layer_size,hidden_layer_size)
-initial_theta2 = randInitializeWeights(hidden_layer_size,num_labels)
-initial_theta = np.append(initial_theta1.flatten(),initial_theta2.flatten())
-print(initial_theta1.shape)
-print(initial_theta2.shape)
-print(initial_theta.shape)
-
-
-theta,J_history = gradientDescent(X,y,initial_theta,0.8,50,1,input_layer_size,hidden_layer_size,num_labels)
-theta1 = theta[:((input_layer_size+1)*hidden_layer_size)].reshape(hidden_layer_size,input_layer_size+1)
-theta2 = theta[((input_layer_size+1)*hidden_layer_size):].reshape(num_labels,hidden_layer_size+1)
-print(theta1.shape)
-print(theta2.shape)
-print(theta.shape)
-
-
-print("tempo:",dt.now() - agora)
-
-pred = prediction(X,theta1,theta2)
-print(pred.shape)
-print(pred[:3])
-print("Training Set Accuracy:",sum(pred[:,np.newaxis]==y)[0]/5000*100,"%")
-"""
