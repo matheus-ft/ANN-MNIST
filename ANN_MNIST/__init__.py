7c35264e8afca87b434e457f5251de66aa41f019
Matheus Feres Turcheti <71662383+matheus-ft@users.noreply.github.com>
2022-10-16 21:03:11 -0300

    Add conjugate gradient method

--- ANN_MNIST/__init__.py
+++ ANN_MNIST/__init__.py
@@ -2,10 +2,12 @@ from .nn import (
     metrics,
     assembly_nn,
     gradientDescent,
+    conjugate_gradient,
     prediction,
     gradient_check,
     theta_meaning,
 )
+
 from .data import (
     get_data,
     split_data,
